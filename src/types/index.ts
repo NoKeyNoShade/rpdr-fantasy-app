f06cea0e52b534cf78837c91a4c68b4ffcd8a4ba
GarrettLevine <garrlevine@gmail.com>
2018-09-22 14:17:24 -0400

    imports input styles into top level types folder

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,8 @@
 import * as Header from '../components/header/types';
+import * as Input from '../components/input/types';
 
 export default {
   HeaderType: Header.HeaderType,
+  InputType: Input.InputType,
+  InputStyle: Input.InputStyle,
 };
